68042cc900df8f4c0905c0243e8204080e14f3fc
yan <yancoding@qq.com>
2021-09-05 14:00:39 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-learn
+# learn
+
+## first
---
+++ index.vue
@@ -0,0 +1,16 @@
+<template lang="">
+<div
+v-text="msg"
+>
+
+</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "content",
+    };
+  },
+};
+</script>
